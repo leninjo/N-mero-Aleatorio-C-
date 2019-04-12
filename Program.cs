65ca62cf3c87c1df9830bdd02b993afa6eda4787
leninjo <31749372+leninjo@users.noreply.github.com>
2019-04-12 09:17:25 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,16 +6,6 @@ namespace practicas.cs
     {
         static void Main(string[] args)
         {
-            /*int numero1 = 1;
-            int numero2 = 2;
-            double c = numero1 + numero2;
-            Console.WriteLine("La suma es: " + c);
-
-            Console.WriteLine("Ingresa un numero: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("El número que ingresaste es: "+num);
-            Console.WriteLine(typeof(num));*/
-            
         Random rnd = new Random();
         int numero = rnd.Next(1,11);
         int intentos = 0;
